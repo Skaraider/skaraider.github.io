019684c90bb2670e0a72bd0c0857a7a088f0ca24
Robert Chambers <114707118+Skaraider@users.noreply.github.com>
2023-08-03 13:45:29 -0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,14 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll('.toggleButton');
+
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             const targetId = button.getAttribute('data-target');
             const targetElement = document.getElementById(targetId);
-            if (targetElement.style.display === "none") {
+
+            if (targetElement.style.display === "none" || targetElement.style.display === '') {
                 targetElement.style.display = "block";
+                updateProgress(targetElement);
             } else {
                 targetElement.style.display = "none";
             }
         });
     });
+
+    function updateProgress(tableElement) {
+        let rows = tableElement.querySelectorAll("tr");
+        for(let i=0; i<rows.length; i++) {
+            let progress = rows[i].querySelector(".progress");
+            let value = rows[i].querySelector(".value");
+    
+            if(progress && value) {
+                progress.style.width = value.innerText;
+            }
+        }
+    }
 });
